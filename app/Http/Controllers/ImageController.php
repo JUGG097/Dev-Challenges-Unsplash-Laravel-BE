525cbda57c544bbf9195530e170e72d389d65acc
Adeoluwa Adeboye <adeneye97@yahoo.com>
2022-06-01 13:14:03 +0100

    :construction: Finished Endpoints, Tested integration with FE

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ImageController extends Controller
 {
-    //
+    public function fetch(Request $req)
+    {
+        $images = Image::orderBy('created_at', 'desc')->get(['_id', 'img_url', 'label']);
+        return response()->json(['success' => true, 'data' => $images]);
+    }
+
+    public function store(Request $req)
+    {
+
+        $validator = Validator::make($req->all(), [
+            'img_url' => 'required|url',
+            'label' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'message' => $validator->errors()], 400);
+        }
+
+        $image = Image::create(['img_url' => $req->img_url, 'label' => $req->label]);
+
+        return response()->json(['success' => true, 'data' => $image]);
+    }
+
+    public function destroy($imageId)
+    {
+        $flight = Image::findorFail($imageId);
+
+        $flight->delete();
+
+        return response()->json(['success' => true, 'data' => []]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,4 +22,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/check', function (Request $request) {
     return response()->json(['success' => true], 200);
+});
+
+Route::prefix('/v1')->group(function () {
+    Route::get('/images', [ImageController::class, 'fetch']);
+    Route::post('/images', [ImageController::class, 'store']);
+    Route::delete('/images/{imageId}', [ImageController::class, 'destroy']);
 });
